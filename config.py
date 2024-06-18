6b80cdd6b8154584e755066b7c13ee035d21d974
Createbots23 <bots14237@gmail.com>
2024-06-18 07:54:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os, re
 
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+API_ID = int(os.environ.get("API_ID", "21740783"))
+API_HASH = os.environ.get("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")
+OWNER = int(os.environ.get("OWNER", "6299192020"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Yolhcyxiy_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002005436412")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002201298270"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Speedwolf1:speedwolf24689@cluster0.rgfywsf.mongodb.net/")
+DB_NAME = os.environ.get("DB_NAME", "Speedwolf1")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+STRING = os.environ.get("STRING", "0")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/14cd2977df4d3ede888e0.jpg")
 
 
 
@@ -22,6 +22,6 @@ BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f62
 
 # if you need to add verify system then dm me on telegram
 
-SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
-SHORTNER_API = os.environ.get("SHORTNER_API", "")
-TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
+SHORTNER_URL = os.environ.get("SHORTNER_URL", "0")
+SHORTNER_API = os.environ.get("SHORTNER_API", "0")
+TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "0")
